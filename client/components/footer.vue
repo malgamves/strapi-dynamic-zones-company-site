640c3099525b468ebabd8fcebad3cb568f952d52
Daniel Phiri <daniel@Daniels-MacBook-Pro.local>
2022-12-12 00:08:07 +0100

    fix: removed require in footer

--- client/components/footer.vue
+++ client/components/footer.vue
@@ -2,7 +2,7 @@
 <div>
     <footer class="footer">
         <div class="container">
-            <span v-html="settings.footerText" /> <span> Thanks to <a href="https://unsplash.com/@wocintechchat">Christina</a>,<a href="https://unsplash.com/@galina88"> Galina </a>and <a href="https://unsplash.com/@bkotynski">Bethany</a> for the images.</span>
+            <span /> Copyright &copy; 2022 |<span> Thanks to <a href="https://unsplash.com/@wocintechchat">Christina</a>,<a href="https://unsplash.com/@galina88"> Galina </a>and <a href="https://unsplash.com/@bkotynski">Bethany</a> for the images.</span>
         </div>
     </footer>
 </div>
@@ -15,7 +15,6 @@ export default {
 </script>
 
 <script setup>
-    const settings = require("../info.json")
         
 </script>
 

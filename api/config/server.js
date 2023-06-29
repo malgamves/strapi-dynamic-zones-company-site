3fce0d799fb72619205f2003a8c62a882f3cd0e3
Atle Stromsvold <obsfeil@outlook.com>
2023-06-30 01:03:46 +0200

    a

--- api/config/server.js
+++ api/config/server.js
@@ -2,6 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    _keys: env.array('APP_KEYS'),
+    get keys() {
+      return this._keys;
+    },
+    set keys(value) {
+      this._keys = value;
+    },
   },
 });
